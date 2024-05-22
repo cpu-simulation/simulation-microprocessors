d6145a4afe621836337bd6faa6102b280e6ab0fb
mohammad-mallaee <mohammad.mallaee@gmail.com>
2024-05-22 20:44:10 +0330

    simplify shift module and fix its bugs

--- src/components/shift.py
+++ src/components/shift.py
@@ -3,27 +3,18 @@ from reversed_index_list import ReversedIndexList
 
 
 class Shift:
-    def __init__(self, register: ReversedIndexList[int], I_r=0, I_l=0):
-        self.register = register
-        self.I_r = I_r
-        self.I_l = I_l
+    def __init__(self, size: int):
+        if size <= 2:
+            raise Exception("Size of shift module cannot be less than 2")
+        self.size = size
+        self.mux = [Mux(2) for _ in range(size)]
 
-        self.size = len(register)  # Number of registers
-        self.mux = [Mux(2) for _ in range(self.size)]
+        self._A: ReversedIndexList
         self._s = [0]
+        self._i = 0
 
-        self.prepare()
-
-    def prepare(self):
-        self.mux[0].i[0] = self.I_r
-        self.mux[0].i[1] = self.register[1]
-
-        for i in range(1, self.size - 1):
-            self.mux[i].i[0] = self.register[i - 1]
-            self.mux[i].i[1] = self.register[i + 1]
-
-        self.mux[-1].i[0] = self.register[-2]
-        self.mux[-1].i[1] = self.I_l
+        self.i = 0
+        self.A = ReversedIndexList([0] * size)
 
     @property
     def s(self):
@@ -35,21 +26,35 @@ class Shift:
             self.mux[i].s = value
 
     @property
-    def shr(self):
-        self.s = [0]
-        return self.out
+    def i(self):
+        return self._i
+
+    @i.setter
+    def i(self, value):
+        self._i = value
+        self.mux[-1].i[0] = value
+        self.mux[0].i[1] = value
 
     @property
-    def shl(self):
-        self.s = [1]
-        return self.out
+    def A(self):
+        return self._A
+
+    @A.setter
+    def A(self, value: ReversedIndexList[int]):
+        self._A = value
+        self.mux[-2].i[0] = value[-1]
+        self.mux[1].i[1] = value[0]
+        for i in range(1, self.size - 1):
+            self.mux[i - 1].i[0] = value[i]
+            self.mux[i + 1].i[1] = value[i]
 
     @property
     def out(self):
-        return ReversedIndexList([mux.out for mux in self.mux])
+        return ReversedIndexList([mux.out for mux in self.mux], True)
 
 
-# register = ReversedIndexList([1, 0, 1, 0])
-# shift = Shift(register)
-# shift.s = [0]  # zeRo for Right, one for left
+# shift = Shift(4)
+# shift.i = 1
+# shift.A = ReversedIndexList([0, 1, 1, 0])
+# shift.s = [0]  # zero for Right, one for left
 # print(shift.out)
