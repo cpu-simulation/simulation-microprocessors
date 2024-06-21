7a384a0f589da3e80ad959e8ef5040c65c23f941
Davoud Nosrati <davwvod@gmail.com>
2024-06-21 17:29:56 +0330

    Fix memory R/W bug

--- backend/cpu_sim/views/memory.py
+++ backend/cpu_sim/views/memory.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseServerError
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from cpu.utils.binary import dec_to_binlist
 
 from ..lazycpu import WorkingCPU
 
@@ -13,16 +14,16 @@ def read_memory(request):
 
 @api_view(["POST"])
 def write_to_memory(request):
-    try:
-        data = request.data
-        for cell_info in data:
-            address = int(cell_info["address"], 16)
-            value = list(str(int(cell_info["value"], 16)))
-            WorkingCPU.memory.write(address=address, data=value)
-        response = read_memory()
-    except Exception:
-        response = Response(
-            data="Internal Server Error", exception=HttpResponseServerError, status=500
-        )
+    # try:
+    data = request.data
+    for cell_info in data:
+        address = int(cell_info["address"], 16)
+        value = dec_to_binlist(int(cell_info["value"], 16))
+        WorkingCPU.memory.write(address=address, data=value)
+    response = Response("OK", 200)
+    # except Exception:
+    #     response = Response(
+    #         data="Internal Server Error", exception=HttpResponseServerError, status=500
+    #     )
 
     return response
